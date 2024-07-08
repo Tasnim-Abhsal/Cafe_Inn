057949de323ad976ba33f364f56dfd22ca72663a
Mithila-CSE <163147131+Mithila-CSE@users.noreply.github.com>
2024-07-08 22:21:44 +0600

    Add files via upload

---
+++ CafeInn/CashierMainForm.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace CafeInn
+{
+    partial class CashierMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.close = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.addProducts_btn = new System.Windows.Forms.Button();
+            this.customer_btn = new System.Windows.Forms.Button();
+            this.dashboard_btn = new System.Windows.Forms.Button();
+            this.order_btn = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.logout_btn = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.cashierOrderForm1 = new CafeInn.CashierOrderForm();
+            this.adminAddProducts1 = new CafeInn.AdminAddProducts();
+            this.adminDashboardForm1 = new CafeInn.AdminDashboardForm();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SeaShell;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.close);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1319, 45);
+            this.panel1.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("French Script MT", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkRed;
+            this.label4.Location = new System.Drawing.Point(74, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 37);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Café Inn";
+            // 
+            // close
+            // 
+            this.close.AutoSize = true;
+            this.close.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close.ForeColor = System.Drawing.Color.Red;
+            this.close.Location = new System.Drawing.Point(1272, 9);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(24, 27);
+            this.close.TabIndex = 15;
+            this.close.Text = "x";
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Tan;
+            this.panel2.Controls.Add(this.addProducts_btn);
+            this.panel2.Controls.Add(this.customer_btn);
+            this.panel2.Controls.Add(this.dashboard_btn);
+            this.panel2.Controls.Add(this.order_btn);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.logout_btn);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 45);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(254, 698);
+            this.panel2.TabIndex = 2;
+            // 
+            // addProducts_btn
+            // 
+            this.addProducts_btn.BackColor = System.Drawing.Color.Transparent;
+            this.addProducts_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addProducts_btn.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addProducts_btn.ForeColor = System.Drawing.Color.DarkRed;
+            this.addProducts_btn.Location = new System.Drawing.Point(30, 343);
+            this.addProducts_btn.Name = "addProducts_btn";
+            this.addProducts_btn.Size = new System.Drawing.Size(206, 46);
+            this.addProducts_btn.TabIndex = 23;
+            this.addProducts_btn.Text = "Add Products";
+            this.addProducts_btn.UseVisualStyleBackColor = false;
+            // 
+            // customer_btn
+            // 
+            this.customer_btn.BackColor = System.Drawing.Color.Transparent;
+            this.customer_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.customer_btn.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customer_btn.ForeColor = System.Drawing.Color.DarkRed;
+            this.customer_btn.Location = new System.Drawing.Point(30, 480);
+            this.customer_btn.Name = "customer_btn";
+            this.customer_btn.Size = new System.Drawing.Size(206, 46);
+            this.customer_btn.TabIndex = 22;
+            this.customer_btn.Text = "Customer";
+            this.customer_btn.UseVisualStyleBackColor = false;
+            // 
+            // dashboard_btn
+            // 
+            this.dashboard_btn.BackColor = System.Drawing.Color.Transparent;
+            this.dashboard_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dashboard_btn.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashboard_btn.ForeColor = System.Drawing.Color.DarkRed;
+            this.dashboard_btn.Location = new System.Drawing.Point(30, 268);
+            this.dashboard_btn.Name = "dashboard_btn";
+            this.dashboard_btn.Size = new System.Drawing.Size(206, 46);
+            this.dashboard_btn.TabIndex = 20;
+            this.dashboard_btn.Text = "Dashboard";
+            this.dashboard_btn.UseVisualStyleBackColor = false;
+            // 
+            // order_btn
+            // 
+            this.order_btn.BackColor = System.Drawing.Color.Transparent;
+            this.order_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.order_btn.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.order_btn.ForeColor = System.Drawing.Color.DarkRed;
+            this.order_btn.Location = new System.Drawing.Point(30, 413);
+            this.order_btn.Name = "order_btn";
+            this.order_btn.Size = new System.Drawing.Size(206, 46);
+            this.order_btn.TabIndex = 19;
+            this.order_btn.Text = "Order";
+            this.order_btn.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Tan;
+            this.pictureBox3.Image = global::CafeInn.Properties.Resources.logout;
+            this.pictureBox3.Location = new System.Drawing.Point(53, 640);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(49, 46);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // logout_btn
+            // 
+            this.logout_btn.BackColor = System.Drawing.Color.Transparent;
+            this.logout_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.logout_btn.Font = new System.Drawing.Font("Georgia", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logout_btn.ForeColor = System.Drawing.Color.DarkRed;
+            this.logout_btn.Location = new System.Drawing.Point(92, 640);
+            this.logout_btn.Name = "logout_btn";
+            this.logout_btn.Size = new System.Drawing.Size(144, 46);
+            this.logout_btn.TabIndex = 18;
+            this.logout_btn.Text = "LogOut";
+            this.logout_btn.UseVisualStyleBackColor = false;
+            this.logout_btn.Click += new System.EventHandler(this.logout_btn_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkRed;
+            this.label6.Location = new System.Drawing.Point(48, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(174, 27);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Cashier\'s Portal";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CafeInn.Properties.Resources.Cup1;
+            this.pictureBox2.Location = new System.Drawing.Point(40, 17);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(183, 155);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.cashierOrderForm1);
+            this.panel3.Controls.Add(this.adminAddProducts1);
+            this.panel3.Controls.Add(this.adminDashboardForm1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(254, 45);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1065, 698);
+            this.panel3.TabIndex = 3;
+            // 
+            // cashierOrderForm1
+            // 
+            this.cashierOrderForm1.BackColor = System.Drawing.Color.White;
+            this.cashierOrderForm1.Location = new System.Drawing.Point(3, 3);
+            this.cashierOrderForm1.Name = "cashierOrderForm1";
+            this.cashierOrderForm1.Size = new System.Drawing.Size(1059, 692);
+            this.cashierOrderForm1.TabIndex = 2;
+            this.cashierOrderForm1.Load += new System.EventHandler(this.cashierOrderForm1_Load);
+            // 
+            // adminAddProducts1
+            // 
+            this.adminAddProducts1.BackColor = System.Drawing.Color.White;
+            this.adminAddProducts1.Location = new System.Drawing.Point(0, 0);
+            this.adminAddProducts1.Name = "adminAddProducts1";
+            this.adminAddProducts1.Size = new System.Drawing.Size(1065, 695);
+            this.adminAddProducts1.TabIndex = 1;
+            // 
+            // adminDashboardForm1
+            // 
+            this.adminDashboardForm1.BackColor = System.Drawing.Color.White;
+            this.adminDashboardForm1.Location = new System.Drawing.Point(3, 0);
+            this.adminDashboardForm1.Name = "adminDashboardForm1";
+            this.adminDashboardForm1.Size = new System.Drawing.Size(1062, 702);
+            this.adminDashboardForm1.TabIndex = 0;
+            // 
+            // CashierMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1319, 743);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CashierMainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CashierMainForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label close;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button addProducts_btn;
+        private System.Windows.Forms.Button customer_btn;
+        private System.Windows.Forms.Button dashboard_btn;
+        private System.Windows.Forms.Button order_btn;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button logout_btn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel3;
+        private AdminAddProducts adminAddProducts1;
+        private AdminDashboardForm adminDashboardForm1;
+        private CashierOrderForm cashierOrderForm1;
+    }
+}
