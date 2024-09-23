fd8c686108b621307d9627151c250bac23aa49dd
Tasnim Abhsal (Turjo) <98803320+Tasnim-Abhsal@users.noreply.github.com>
2024-09-23 21:08:30 +0600

    Add files via upload

---
+++ CafeInn/CustomersData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.Remoting.Messaging;
+
+namespace CafeInn
+{
+    class CustomersData
+    {
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\My Lenovo\Documents\cafeIn.mdf"";Integrated Security=True;Connect Timeout=30");
+        public int CustomerID { get; set; }
+        public string TotalPrice { get; set; }
+        public string Amount { get; set; }
+        public string Change { get; set; }
+        public string Date { get; set; }
+        public List<CustomersData>  allCustomersData()
+        {
+            List<CustomersData> listData = new List<CustomersData>();
+
+            if (connect.State == ConnectionState.Closed)
+            {
+                try
+                {
+                    connect.Open();
+                    string selectData = "SELECT * FROM customers";
+                    using (SqlCommand cmd = new SqlCommand(selectData, connect))
+                    {
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            CustomersData cData = new CustomersData();
+                            cData.CustomerID = (int)reader["customer_id"];
+                            cData.TotalPrice = reader["total_price"].ToString();
+                            cData.Amount = reader["amount"].ToString();
+                            cData.Change = reader["change"].ToString();
+                            cData.Date = reader["date"].ToString();
+                            listData.Add(cData);
+                        }
+
+                    }
+                } catch (Exception ex)
+                {
+                    Console.WriteLine("Connection Failed: " + ex);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+
+            }
+            return listData;
+
+        }
+
+    }
+
+
+
+
+}
+
